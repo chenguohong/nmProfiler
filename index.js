bb90d673648fa7dd114bae93f5287e1d0ce99325
chenguohong <39668796+chenguohong@users.noreply.github.com>
2018-06-17 17:56:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
-const nmProfiler = require('./dispatch');
-//nmProfiler({
+const node_monitor_profiler = require('./dispatch');
+//node_monitor_profiler({
 //    project_name: 'Game Boy',
 //    embrace: {
 //    tcp_host: '127.0.0.1',
 //        tcp_port: 30000
 //}
 //})
-module.exports = nmProfiler;
+module.exports = node_monitor_profiler;
